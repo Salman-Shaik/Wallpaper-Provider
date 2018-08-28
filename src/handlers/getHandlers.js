8023c818625b09b48235b1b4d81b867751bf2332
Salman-Shaik <dukesalman5@gmail.com>
2018-08-29 01:42:59 +0530

    renamed gethandlers to getHandlers
    What changes have you made?
    
    *
    
    Why do we need this change?
    
    *
    
    What can this change impact?

--- src/handlers/getHandlers.js
+++ src/handlers/getHandlers.js
@@ -1,7 +1,6 @@
 let utilities = require('../library/utils');
 const fs = require('fs');
 const path = require('path');
-
 const getWallpaper = (req, res) => {
   res.sendFile(path.join(__dirname,
     '../../public/Resources/images',
